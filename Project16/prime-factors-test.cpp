fbff8825a5cb4ff7fffa7ed07c4316aa945737d7
yulgon <younggon5548@gmail.com>
2025-07-23 09:56:14 +0900

    [feature] 2

--- Project16/prime-factors-test.cpp
+++ Project16/prime-factors-test.cpp
@@ -7,3 +7,10 @@ TEST(PrimeFactors, PrimeTest)
     vector<int> expected = {};
     EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFactors, Of2)
+{
+    PrimeFactors prime_factor;
+    vector<int> expected = {2};
+    EXPECT_EQ(expected, prime_factor.of(2));
+}
