0a1573cdab5063213bd7f4742336b718b3c5a236
yulgon <younggon5548@gmail.com>
2025-07-23 10:11:26 +0900

    [feature] 7

--- Project16/prime-factors-test.cpp
+++ Project16/prime-factors-test.cpp
@@ -43,3 +43,9 @@ TEST_F(PrimeFixture, Of9)
     expected = { 3, 3 };
     EXPECT_EQ(expected, prime_factor.of(9));
 }
+
+TEST_F(PrimeFixture, Of12)
+{
+    expected = { 2, 2, 3 };
+    EXPECT_EQ(expected, prime_factor.of(12));
+}
